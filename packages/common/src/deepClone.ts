795ce5e999a4bea5ae7198864ebcf30e748c798d
ringeringeraja <joaosan177@gmail.com>
2024-03-18 22:18:20 -0300

    fix(common): deepClone() bug

--- packages/common/src/deepClone.ts
+++ packages/common/src/deepClone.ts
@@ -1,5 +1,5 @@
 export const deepClone = <const TObject>(obj: TObject): TObject => {
-  return typeof 'structuredClone' === 'function'
+  return typeof structuredClone === 'function'
     ? structuredClone(obj)
     : JSON.parse(JSON.stringify(obj))
 }
