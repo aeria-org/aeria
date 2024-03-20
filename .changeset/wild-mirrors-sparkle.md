1f97acefeaa977915c8fa78c67847e7047f99531
ringeringeraja <joaosan177@gmail.com>
2024-03-19 21:59:35 -0300

    fix(common): prefer JSON.stringify() over structuredClone() on browsers

---
+++ .changeset/wild-mirrors-sparkle.md
@@ -0,0 +1,5 @@
+---
+"@aeriajs/common": patch
+---
+
+Prefer JSON.stringify() over structuredClone() on browsers
--- packages/common/src/deepClone.ts
+++ packages/common/src/deepClone.ts
@@ -1,5 +1,6 @@
 export const deepClone = <const TObject>(obj: TObject): TObject => {
-  return typeof structuredClone === 'function'
+  return typeof structuredClone === 'function' && typeof window === 'undefined'
     ? structuredClone(obj)
     : JSON.parse(JSON.stringify(obj))
 }
+
