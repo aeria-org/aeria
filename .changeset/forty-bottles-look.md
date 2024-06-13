afbd76e4f9d18159901ce80a3a07a88897e3b386
ringeringeraja <joaosan177@gmail.com>
2024-06-12 23:46:55 -0300

    feat(common): make `deserialize()` generic

---
+++ .changeset/forty-bottles-look.md
@@ -0,0 +1,5 @@
+---
+"@aeriajs/common": patch
+---
+
+Make `deserialize()` generic
--- packages/common/src/serialize.ts
+++ packages/common/src/serialize.ts
@@ -2,6 +2,7 @@ import * as BSON from 'bson'
 
 export const serialize = (...args: Parameters<typeof BSON.serialize>) => Buffer.from(BSON.serialize(...args)).toString('latin1')
 
-export const deserialize = <T extends ReturnType<typeof serialize>>(buffer: T) => {
-  return BSON.deserialize(new Uint8Array(Array.from(buffer).map((c) => c.charCodeAt(0))))
+export const deserialize = <TValue>(buffer: ReturnType<typeof serialize>) => {
+  return BSON.deserialize(new Uint8Array(Array.from(buffer).map((c) => c.charCodeAt(0)))) as TValue
 }
+
