28525e846665b97d9f50eb01baca2afe79749a25
ringeringeraja <joaosan177@gmail.com>
2024-07-02 00:19:06 -0300

    fix(core): fill insert result

---
+++ .changeset/fast-boxes-clean.md
@@ -0,0 +1,5 @@
+---
+"@aeriajs/core": patch
+---
+
+Fill insert result
--- packages/core/src/functions/builtin/insert.ts
+++ packages/core/src/functions/builtin/insert.ts
@@ -2,7 +2,7 @@ import type { Context, SchemaWithId, InsertPayload, InsertReturnType } from '@ae
 import { Result, HTTPStatus, ACError, ValidationErrorCode } from '@aeriajs/types'
 import { useSecurity } from '@aeriajs/security'
 import { throwIfError, endpointErrorSchema } from '@aeriajs/common'
-import { traverseDocument, normalizeProjection, prepareInsert } from '../../collection/index.js'
+import { traverseDocument, normalizeProjection, prepareInsert, fill } from '../../collection/index.js'
 
 export type InsertOptions = {
   bypassSecurity?: boolean
@@ -115,11 +115,11 @@ export const insert = async <TContext extends Context>(
     })
   }
 
-  const result = throwIfError(await traverseDocument(doc, context.description, {
+  const result = fill(throwIfError(await traverseDocument(doc, context.description, {
     getters: true,
     fromProperties: true,
     recurseReferences: true,
-  }))
+  })), context.description)
 
   return Result.result(result)
 }
