50a5fd62b6bd74de3e2209c71e08829b3d8d94e0
ringeringeraja <joaosan177@gmail.com>
2024-08-22 19:58:05 -0300

    refactor: `insertUser()` signature

--- packages/builtins/src/collections/user/insert.ts
+++ packages/builtins/src/collections/user/insert.ts
@@ -1,11 +1,10 @@
 import type { Context, SchemaWithId, InsertPayload, Description } from '@aeriajs/types'
-import type { description } from './description.js'
 import { insert as originalInsert } from '@aeriajs/core'
 import * as bcrypt from 'bcrypt'
 
 export const insert = async <
-  TDescription extends Description = typeof description,
-  TInsertPayload extends InsertPayload<SchemaWithId<TDescription>> = InsertPayload<SchemaWithId<TDescription>>,
+  TDescription extends Description,
+  TInsertPayload extends InsertPayload<SchemaWithId<TDescription>>,
 >(
   payload: NoInfer<TInsertPayload>,
   context: Context<TDescription>,
