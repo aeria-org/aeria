488626a1fc28c8e9ab6825cb9f8c5844d239b415
ringeringeraja <joaosan177@gmail.com>
2024-10-04 03:07:00 -0300

    refactor(core): remove `createAeria()`

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,7 +6,6 @@ export * from './database.js'
 export * from './functions/index.js'
 export * from './endpoints.js'
 export * from './token.js'
-export * from './use.js'
 export * from './functions/index.js'
 export * as functions from './functions/index.js'
 export {
