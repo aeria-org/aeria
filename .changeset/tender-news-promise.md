af7c82038504552e60e6db4388abfe193e06ed22
ringeringeraja <joaosan177@gmail.com>
2024-10-16 19:57:17 -0300

    fix(builtins): `file.remove()` access control

---
+++ .changeset/tender-news-promise.md
@@ -0,0 +1,5 @@
+---
+"@aeriajs/builtins": patch
+---
+
+Fix `file.remove()` access control
--- packages/builtins/src/collections/file/remove.ts
+++ packages/builtins/src/collections/file/remove.ts
@@ -1,19 +1,23 @@
 import type { Context, SchemaWithId, RemovePayload } from '@aeriajs/types'
 import type { description } from './description.js'
-import { remove as originalRemove, type ObjectId } from '@aeriajs/core'
+import { Result } from '@aeriajs/types'
+import { remove as originalRemove, get, type ObjectId } from '@aeriajs/core'
 import * as fs from 'fs/promises'
 
-export const remove = async (payload: RemovePayload<SchemaWithId<typeof description>>, context: Context<typeof description>) => {
-  const file = await context.collection.model.findOne({
-    _id: <ObjectId>payload.filters._id,
-  }, {
-    projection: {
-      absolute_path: 1,
+export const remove = async (
+payload: RemovePayload<SchemaWithId<typeof description>>, context: Context<typeof description>
+) => {
+  const { error, result: file } = await get({
+    filters: {
+      _id: <ObjectId>payload.filters._id,
     },
-  })
+    project: [
+      'absolute_path',
+    ],
+  }, context)
 
-  if( !file ) {
-    throw new Error('')
+  if( error ) {
+    return Result.error(error)
   }
 
   try {
