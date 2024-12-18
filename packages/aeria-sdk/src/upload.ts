b30deb654d3b72b8cc035042c11ad8c647fecd70
ringeringeraja <joaosan177@gmail.com>
2024-12-18 15:08:06 -0300

    refactor(aeria-sdk): remove node dependency from upload

--- packages/aeria-sdk/src/upload.ts
+++ packages/aeria-sdk/src/upload.ts
@@ -2,7 +2,6 @@ import type { Result, EndpointError, TempId } from '@aeriajs/types'
 import type { RequestConfig } from '@aeriajs/common'
 import type { FileMetadata, UploadHeaders } from '@aeriajs/core'
 import type { InstanceConfig } from './types.js'
-import * as querystring from 'node:querystring'
 import { request } from './http.js'
 import { publicUrl } from './utils.js'
 
@@ -16,12 +15,12 @@ type UploadParams = {
 }
 
 export const uploader = (config: InstanceConfig) => (bearerToken?: string) => async (collectionName: keyof Collections, params: UploadParams) => {
-  const qs = querystring.encode({
+  const qs = {
     name: params.name,
-    format: params.format || 'raw' satisfies UploadParams['format'],
-  } satisfies typeof FileMetadata)
+    format: params.format || 'raw'
+  } satisfies typeof FileMetadata
 
-  const url = `${publicUrl(config)}/${collectionName}/upload?${qs}`
+  const url = `${publicUrl(config)}/${collectionName}/upload?name=${qs.name}&format=${qs.format}`
 
   const headers: typeof UploadHeaders & {
     authorization?: `Bearer ${string}`
