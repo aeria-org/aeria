3d2a4de5e82aed7750cb3e4e4648774625651811
Cephas369 <pedro-leitao@outlook.com>
2024-12-28 12:37:50 -0400

    fix: add contract export to aeria

--- packages/compiler/.aeria-test/out/index.js
+++ packages/compiler/.aeria-test/out/index.js
@@ -1,2 +1,3 @@
 export * as collections from './collections/index.js'
 export { extendFileCollection, extendTempFileCollection, extendUserCollection, extendAnimalCollection, extendPetCollection } from './collections/collections.js'
+export * from './collections/contracts.js'
