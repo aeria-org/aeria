3966212ca4689dbcad4a073ee4be2e77193c7ba4
samuel caliman gonçalves <samuelcaliman@gmail.com>
2024-12-30 18:53:30 -0300

    feat: add rules to strengthen security of user insert

--- packages/builtins/src/collections/user/insert.ts
+++ packages/builtins/src/collections/user/insert.ts
@@ -1,4 +1,4 @@
-import type { Context, SchemaWithId, InsertPayload, Description } from '@aeriajs/types'
+import { type Context, type SchemaWithId, type InsertPayload, type Description, HTTPStatus, ACError } from '@aeriajs/types'
 import { insert as originalInsert } from '@aeriajs/core'
 import * as bcrypt from 'bcrypt'
 
@@ -9,10 +9,48 @@ export const insert = async <
   payload: NoInfer<TInsertPayload>,
   context: Context<TDescription>,
 ) => {
+  if(!context.token.sub){ 
+    return context.error(HTTPStatus.Unauthorized, {code:ACError.AuthenticationError})
+  }
   if( 'password' in payload.what && typeof payload.what.password === 'string' ) {
     payload.what.password = await bcrypt.hash(payload.what.password, 10)
   }
-
+  if(('email' in payload.what)){
+    if(!(typeof payload.what.email === 'string')){
+      return context.error(HTTPStatus.UnprocessableContent, {code:ACError.MalformedInput})
+    }
+    const existing = await context.collections.user.model.findOne({
+      email: payload.what.email
+    })
+    if(existing && existing.email !== context.token.userinfo.email){
+      const {error: noUser, result:user} = await context.collections.user.functions.get({
+        filters:{
+          _id: context.token.sub
+        }
+      })
+      if(noUser){
+        return context.error(HTTPStatus.Forbidden, {code:ACError.ResourceNotFound})
+      }
+      if(existing.email !== user.email){
+        return context.error(HTTPStatus.Forbidden, {code:ACError.OwnershipError})
+      }
+    }
+  }
+  if(!context.token.roles.includes('root')){
+    if('_id' in payload.what){
+      if(payload.what._id !== context.token.sub.toString()){
+        console.log('different ID')
+        return context.error(HTTPStatus.Unauthorized, {code:ACError.AuthorizationError})
+      }
+      if('roles' in payload.what){
+        payload.what.roles = context.token.roles
+      }
+      
+      return originalInsert(payload, context)
+    }
+    console.log('not root')
+    return context.error(HTTPStatus.Unauthorized, {code:ACError.AuthorizationError})
+  }
   return originalInsert(payload, context)
 }
 
