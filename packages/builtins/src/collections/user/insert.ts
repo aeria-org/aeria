91a51c4b8582d729d4b1e53884e739c8eefd56b7
samuel caliman gonçalves <samuelcaliman@gmail.com>
2025-01-03 19:12:21 -0300

    feat: move user self edit from insert function to standalone edit profile function

--- packages/builtins/src/collections/user/insert.ts
+++ packages/builtins/src/collections/user/insert.ts
@@ -9,50 +9,8 @@ export const insert = async <
   payload: NoInfer<TInsertPayload>,
   context: Context<TDescription>,
 ) => {
-  const mutableProperties = context.config.security.mutableUserProperties
-  if(!context.token.authenticated){
-    throw new Error('function not avaliable for unauthenticated users')
-  }
-
   if( 'password' in payload.what && typeof payload.what.password === 'string' ) {
     payload.what.password = await bcrypt.hash(payload.what.password, 10)
-  }
-
-  if(!context.token.roles.includes('root')){
-    if(!context.token.sub){
-      throw new Error('function not avaliable for unauthenticated users')
-    }
-
-    const user = await context.collections.user.model.findOne({
-      _id: context.token.sub,
-    })
-
-    if(!user){
-      throw new Error('INVALID_TOKEN_USER_ID')
-    }
-
-    const whatPropKeyArray = Object.keys(payload.what).filter((prop) => prop !== '_id')
-    const hasImmutableProps = whatPropKeyArray.some((prop) => !(mutableProperties.includes(prop as typeof mutableProperties[number])))
-
-    if(hasImmutableProps){
-      return context.error(HTTPStatus.Forbidden, {
-        code: ACError.TargetImmutable,
-      })
-    }
-
-    if('_id' in payload.what){
-      if(payload.what._id !== context.token.sub.toString()){
-        return context.error(HTTPStatus.Unauthorized, {
-          code: ACError.AuthorizationError,
-        })
-      }
-
-      return originalInsert(payload, context)
-    }
-
-    return context.error(HTTPStatus.Unauthorized, {
-      code: ACError.AuthorizationError,
-    })
   }
 
   return originalInsert(payload, context)
