3962051f0b52470bc21aacbb7f06a3bad9671169
SamCaliman <151569429+SamCaliman@users.noreply.github.com>
2025-01-03 23:43:28 -0300

    move user self edit from insert to standalone editProfile function (#235)
    * feat: add rules to strengthen security of user insert
    
    * feat: add mutable user properties config for better access control
    
    * feat: add changeset and run lint fix
    
    * feat: refactored email check and fixed types
    
    * fix: mode porperty check to an constant for better readability
    
    * fix: changed name of variable that verifies if an email already exists
    
    * chore: run lint fix
    
    * feat: major refactor for requested changes and update changeset
    
    * chore: remove redundant role payload verification
    
    * chore: run lint fix
    
    * chore: remove signupRequired prop from config
    
    * feat: remove email verification from user insert and create account functions
    
    * feat: move user self edit from insert function to standalone edit profile function
    
    * chore: run lint fix

---
+++ packages/builtins/src/collections/user/editProfile.ts
@@ -0,0 +1,58 @@
+import { type Context, type SchemaWithId, HTTPStatus, ACError, type PackReferences } from '@aeriajs/types'
+import { insert as originalInsert } from '@aeriajs/core'
+import * as bcrypt from 'bcrypt'
+import { type description } from './description.js'
+
+export const editProfile = async (
+  payload: Partial<PackReferences<SchemaWithId<typeof description>>> & Record<string, unknown>,
+  context: Context<typeof description>,
+) => {
+  const mutableProperties = context.config.security.mutableUserProperties
+  if(!context.token.authenticated || !context.token.sub){
+    throw new Error('function not avaliable for unauthenticated users')
+  }
+
+  if(!payload._id ){
+    return context.error(HTTPStatus.BadRequest, {
+      code: ACError.MalformedInput,
+    })
+  }
+
+  if(!context.token.sub.equals(payload._id)){
+    return context.error(HTTPStatus.Unauthorized, {
+      code: ACError.AuthorizationError,
+    })
+  }
+
+  const user = await context.collections.user.model.findOne({
+    _id: context.token.sub,
+  })
+
+  if(!user){
+    throw new Error('INVALID_TOKEN_USER_ID')
+  }
+
+  if( payload.password && typeof payload.password === 'string' ) {
+    payload.password = await bcrypt.hash(payload.password, 10)
+  }
+
+  const whatPropKeyArray = Object.keys(payload).filter((prop) => prop !== '_id')
+  const hasImmutableProps = whatPropKeyArray.some((prop) => !(mutableProperties.includes(prop as typeof mutableProperties[number])))
+
+  if(hasImmutableProps){
+    return context.error(HTTPStatus.Forbidden, {
+      code: ACError.TargetImmutable,
+    })
+  }
+
+  return originalInsert(
+    {
+      what: {
+        _id: payload._id,
+        ...payload,
+      },
+    }
+    , context,
+  )
+
+}
