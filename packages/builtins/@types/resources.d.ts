169afb97f3f43593c0256a38476269091699e87c
ringeringeraja <joaosan177@gmail.com>
2025-07-05 20:31:54 -0300

    chore: deprecate commonjs support

--- packages/builtins/@types/resources.d.ts
+++ packages/builtins/@types/resources.d.ts
@@ -1,5 +1,5 @@
 import type { Collection } from '@aeriajs/types'
 
 declare global {
-  type Collections = typeof import('../src/collections')
+  type Collections = typeof import('../src/collections/index.js')
 }
--- scripts/build.sh
+++ scripts/build.sh
@@ -12,8 +12,8 @@ pnpm --filter='./packages/types' build \
   && pnpm --filter='./packages/core' build \
   && pnpm --filter='./packages/builtins' build \
   && pnpm --filter='./packages/server' build \
-  && pnpm --filter='./packages/aeria-sdk' build \
   && pnpm --filter='./packages/compiler' build \
+  && pnpm --filter='./packages/aeria-sdk' build \
   && pnpm --filter='./packages/cli' build \
   && pnpm --filter='./packages/aeria' build
 
