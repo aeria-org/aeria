e68606fd413427e0e2436b0f85d6d2528de257c6
ringeringeraja <joaosan177@gmail.com>
2025-09-10 16:16:01 -0300

    feat(core): use `freshItem()` in `defineDescriptionTuple()`

--- packages/core/src/collection/description.ts
+++ packages/core/src/collection/description.ts
@@ -1,11 +1,12 @@
 import type { Description, SchemaWithId } from '@aeriajs/types'
+import { freshItem } from '@aeriajs/common'
 
 export const defineDescription = <const TDescription extends Description<TDescription>>(description: TDescription) => {
   return description
 }
 
-export const defineDescriptionTuple = <const TDescription extends Description<TDescription>>(description: Partial<TDescription>) => [
-  {},
+export const defineDescriptionTuple = <const TDescription extends Description<TDescription>>(description: TDescription) => [
+  freshItem(description),
   description,
 ] as unknown as [
   SchemaWithId<TDescription>,
